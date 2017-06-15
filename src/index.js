a12da8f095b67dc2f2e5ed3b979de7a97ffdc13a
Tooraj Khatibi <2rajpx@gmail.com>
2017-06-15 15:09:28 +0430

    Add createState closure
    It returns actions, selectors, reducer, createHoC and createPropTypes based on the recieved stateName and initialState

--- src/index.js
+++ src/index.js
@@ -7,4 +7,121 @@
  * LICENSE.txt file in the root directory of this source tree.
  */
 
-export Greeting from './Greeting.js';
+import { bool, func } from 'prop-types';
+import { connect } from 'react-redux';
+
+const createState = (stateName, initialState = {}) => {
+  /**
+   * ============================================================================
+   * Actions
+   * ============================================================================
+   */
+  const TOGGLE = 'TOGGLE';
+  const toggle = flag => ({
+    type: TOGGLE,
+    stateName,
+    flag,
+  });
+  const ACTIVATE = 'ACTIVATE';
+  const activate = flag => ({
+    type: ACTIVATE,
+    stateName,
+    flag,
+  });
+  const DEACTIVATE = 'DEACTIVATE';
+  const deactivate = flag => ({
+    type: DEACTIVATE,
+    stateName,
+    flag,
+  });
+
+  /**
+   * ============================================================================
+   * Selectors
+   * ============================================================================
+   */
+  const isActive = (state = initialState, flag) => !!state[flag];
+  const isDeactive = (state = initialState, flag) => !isActive(state, flag);
+
+  /**
+   * ============================================================================
+   * Reducer
+   * ============================================================================
+   */
+  const reducer = (state = initialState, action) => {
+    if (stateName !== action.stateName) {
+      return state;
+    }
+    switch (action.type) {
+      case TOGGLE: {
+        const newState = { ...state };
+        newState[action.flag] = !state[action.flag];
+        return newState;
+      }
+      case ACTIVATE: {
+        const newState = { ...state };
+        newState[action.flag] = true;
+        return newState;
+      }
+      case DEACTIVATE: {
+        const newState = { ...state };
+        newState[action.flag] = false;
+        return newState;
+      }
+      default: {
+        return state;
+      }
+    }
+  };
+
+  /**
+   * ============================================================================
+   * Higer-order Component (HOC)
+   * ============================================================================
+   */
+  const createHoC = (flags) => (component) => {
+    const mapStateToProps = (state) => {
+      const props = {};
+      flags.forEach((flag) => {
+        props[`${flag}Value`] = isActive(state[stateName], flag);
+      });
+      return props;
+    };
+    const mapDispatchToProps = (dispatch) => {
+      const props = {};
+      flags.forEach((flag) => {
+        props[`${flag}Handler`] = () => dispatch(toggle(flag));
+      });
+      return props;
+    };
+    return connect(mapStateToProps, mapDispatchToProps)(component);
+  };
+
+  /**
+   * ============================================================================
+   * Prop Types
+   * ============================================================================
+   */
+  const createPropTypes = (
+    flag,
+    isValueRequired = false,
+    isHandlerRequired = false
+  ) => {
+    const propTypes = {};
+    propTypes[`${flag}Value`] = isValueRequired ? bool.isRequired : bool;
+    propTypes[`${flag}Handler`] = isHandlerRequired ? func.isRequired : func;
+    return propTypes;
+  };
+
+  return {
+    TOGGLE, toggle,
+    ACTIVATE, activate,
+    DEACTIVATE, deactivate,
+    isActive, isDeactive,
+    reducer,
+    createHoC,
+    createPropTypes,
+  };
+};
+
+export default createState;
